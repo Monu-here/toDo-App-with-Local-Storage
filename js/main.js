608efd8152f0b3f8afde247601764cec30bb0d56
Monu-here <tmonu995@gmail.com>
2023-05-21 23:14:46 +0545

    todo-app-with-local-storage

--- js/main.js
+++ js/main.js
@@ -99,7 +99,7 @@ function createItem(item) {
 function displayDate() {
     let date = new Date()
     date = date.toString().split(" ")
-    document.querySelector("#date").innerHTML = date [1] + " " + date[2] + " " + date[3]
+    document.querySelector("#date").innerHTML = date [1] + " " + date[2] + " " + date[3] 
 }
 window.onload = function(){
     displayDate()
